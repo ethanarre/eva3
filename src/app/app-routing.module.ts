c9ff3091d11c12680ea21935b43f4100b58a7e60
ethanarre <ethan.arredondo@tecsup.edu.pe>
2024-05-30 23:53:56 -0500

    paginas inicio, nosotros y servicios terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ContactenosComponent } from './components/contactenos/contactenos.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  { path: 'inicio', component: InicioComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'marcas', component: MarcasComponent },
+  { path: 'contactenos', component: ContactenosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
+import { ContactenosComponent } from './components/contactenos/contactenos.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    ProductosComponent,
+    MarcasComponent,
+    ContactenosComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
